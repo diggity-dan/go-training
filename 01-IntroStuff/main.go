826acd5b2ac42ff329a185c5cc7d60c36de13726
Dan <diggity-dan@users.noreply.github.com>
2018-02-25 16:31:52 -0500

    import config file

--- 01-IntroStuff/main.go
+++ 01-IntroStuff/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
+type configuration struct {
+	url    string `json: "url"`
+	port   int    `json: "port"`
+	dburl  string `json: "dburl"`
+	dbname string `json: "dbmame"`
+}
+
+//create a new global config:
+var config configuration
+
 func main() {
-	fmt.Println("Hello World!")
+
+	//get the configuration file from disk:
+	file, err := os.Open("./config/config.json")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(config.port)
+
 }
